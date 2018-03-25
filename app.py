756f1db18ff598038ba661f44592017054446f01
weitingwlin <weiting.lin.lin@gmail.com>
2018-03-25 12:31:38 -0700

    add form

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def about():
   return render_template('about.html')
 
+@app.route('/form')
+def form():
+  return render_template('form.html')
+
 if __name__ == '__main__':
   app.run(port=33507)
