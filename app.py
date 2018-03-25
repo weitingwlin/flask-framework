8f4e612e9a0c7c33e7cd538aa6243b3008a1d281
weitingwlin <weiting.lin.lin@gmail.com>
2018-03-25 13:40:09 -0700

    bokeh circles

--- app.py
+++ app.py
@@ -5,22 +5,23 @@ app = Flask(__name__)
 ticker = 'A'
 
 #########
-import quandl
-from datetime import datetime,date, timedelta
-today = datetime.today()
-lastmonth = today - timedelta(days=30)
-
-quandl.ApiConfig.api_key = 'usn6zz-R-PvDkbuLFbJ2'
-df = quandl.get_table('WIKI/PRICES', ticker='A')
-
-myts = df[(df['date'] >= lastmonth) & (df['date'] < today)][['date','close']]
+# import quandl
+# from datetime import datetime,date, timedelta
+# today = datetime.today()
+# lastmonth = today - timedelta(days=30)
+#
+# quandl.ApiConfig.api_key = 'usn6zz-R-PvDkbuLFbJ2'
+# df = quandl.get_table('WIKI/PRICES', ticker='A')
+#
+# myts = df[(df['date'] >= lastmonth) & (df['date'] < today)][['date','close']]
 
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import file_html
 
 plot = figure()
-plot.circle(myts['date'], myts['close'])
+# plot.circle(myts['date'], myts['close'])
+plot.circle([1,2], [3.4])
 
 html_plot = file_html(plot, CDN, "my plot")
 Html_file= open("templates/plot.html","w")
